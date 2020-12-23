357e6b80b53bcd7488555679eaf01604c67b6858
agaperra <agaperra@yandex.ru>
2020-12-23 15:03:08 +0300

    добавлен скролл, теперь даже если что-то не помещается в экран, то можно проскроллить

--- app/src/main/java/com/geekbrains/android_lessons/MainActivity.java
+++ app/src/main/java/com/geekbrains/android_lessons/MainActivity.java
@@ -4,25 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.activity_main);
+
+
+
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         setTitle("");
         return true;
     }
+}
 
     /*@SuppressLint("ClickableViewAccessibility")
     public static void buttonEffect(View button){
@@ -43,6 +51,3 @@ public class MainActivity extends AppCompatActivity {
         });
         
     }*/
-
-
-}
