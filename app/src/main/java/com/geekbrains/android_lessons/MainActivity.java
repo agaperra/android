0bacd9d4f4ea3b79ebccdd943d5c53a6895dcf59
agaperra <agaperra@yandex.ru>
2020-12-30 10:49:50 +0300

    - изменение тем

--- app/src/main/java/com/geekbrains/android_lessons/MainActivity.java
+++ app/src/main/java/com/geekbrains/android_lessons/MainActivity.java
@@ -2,80 +2,105 @@ package com.geekbrains.android_lessons;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView typeWeatherView;
+    private String typeWeather;
+
+    private TextView degreesView;
+    private int degrees;
+
+    private TextView areaView;
+    private String area;
+
+    private TextView cityView;
+    private String city;
+
+    private TextView windForceView;
+    private double windForce;
+
+    private TextView humidityView;
+    private byte humidity;
+
+    private TextView pressureView;
+    private short pressure;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("INFO: ","onCreate");
-        Toast.makeText(this,"onCreate",Toast.LENGTH_SHORT).show();
+        Log.d("INFO: ", "onCreate");
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+
 
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         setTitle("");
         return true;
     }
+
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("INFO: ","onStart");
-        Toast.makeText(this,"onStart",Toast.LENGTH_SHORT).show();
+        Log.d("INFO: ", "onStart");
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("INFO: ","onResume");
-        Toast.makeText(this,"onResume",Toast.LENGTH_SHORT).show();
+        Log.d("INFO: ", "onResume");
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d("INFO: ","onPause");
-        Toast.makeText(this,"onPause",Toast.LENGTH_SHORT).show();
+        Log.d("INFO: ", "onPause");
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d("INFO: ","onStop");
-        Toast.makeText(this,"onStop",Toast.LENGTH_SHORT).show();
+        Log.d("INFO: ", "onStop");
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d("INFO: ","onRestart");
-        Toast.makeText(this,"onRestart",Toast.LENGTH_SHORT).show();
+        Log.d("INFO: ", "onRestart");
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d("INFO: ","onDestroy");
-        Toast.makeText(this,"onDestroy",Toast.LENGTH_SHORT).show();
+        Log.d("INFO: ", "onDestroy");
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
     }
-
 
 
     @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId())
-        {
+        switch (item.getItemId()) {
             case R.id.ic_menu:
 
                 Intent intentMenu = new Intent(this, SearchActivity.class);
@@ -91,22 +116,4 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 
-    /*@SuppressLint("ClickableViewAccessibility")
-    public static void buttonEffect(View button){
-        button.setOnTouchListener((v, event) -> {
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN: {
-                    v.getBackground().setColorFilter(0xe0858585, PorterDuff.Mode.SRC_ATOP);
-                    v.invalidate();
-                    break;
-                }
-                case MotionEvent.ACTION_UP: {
-                    v.getBackground().clearColorFilter();
-                    v.invalidate();
-                    break;
-                }
-            }
-            return false;
-        });
-        
-    }*/
+
--- app/src/main/java/com/geekbrains/android_lessons/SettingsActivity.java
+++ app/src/main/java/com/geekbrains/android_lessons/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.geekbrains.android_lessons;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.res.Resources;
@@ -21,57 +22,62 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        Log.d("INFO: ","onCreate");
-        Toast.makeText(this,"onCreate",Toast.LENGTH_SHORT).show();
+        Log.d("INFO: ", "onCreate");
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        setTitle("");
+        return true;
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("INFO: ","onStart");
-        Toast.makeText(this,"onStart",Toast.LENGTH_SHORT).show();
+        Log.d("INFO: ", "onStart");
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("INFO: ","onResume");
-        Toast.makeText(this,"onResume",Toast.LENGTH_SHORT).show();
+        Log.d("INFO: ", "onResume");
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d("INFO: ","onPause");
-        Toast.makeText(this,"onPause",Toast.LENGTH_SHORT).show();
+        Log.d("INFO: ", "onPause");
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d("INFO: ","onStop");
-        Toast.makeText(this,"onStop",Toast.LENGTH_SHORT).show();
+        Log.d("INFO: ", "onStop");
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d("INFO: ","onRestart");
-        Toast.makeText(this,"onRestart",Toast.LENGTH_SHORT).show();
+        Log.d("INFO: ", "onRestart");
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d("INFO: ","onDestroy");
-        Toast.makeText(this,"onDestroy",Toast.LENGTH_SHORT).show();
+        Log.d("INFO: ", "onDestroy");
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
     }
 
     @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId())
-        {
+        switch (item.getItemId()) {
             case R.id.ic_menu:
 
                 Intent intentMenu = new Intent(this, SearchActivity.class);
